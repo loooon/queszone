39ca8d0e6c00fd60c38b8295f94faba210d536cd
xp <13289341606@163.com>
2019-05-11 10:48:41 +0800

    创建工程

---
+++ src/main/java/com/xp/queszone/QueszoneApplication.java
@@ -0,0 +1,13 @@
+package com.xp.queszone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueszoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueszoneApplication.class, args);
+    }
+
+}
